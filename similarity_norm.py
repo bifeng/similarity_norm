084fb2ee1e84cc122ff8b6e5a3446ae831208e38
Administrator <hubifengmail@163.com>
2018-10-15 22:48:48 +0800

    second

--- similarity_norm.py
+++ similarity_norm.py
@@ -8,18 +8,33 @@ import numpy as np
 
 # Solution:
 # 1, Reference _predict_proba_lr function
-def softmax(X):
+def softmax(X, copy=True):
+    '''
+    1/(exp(-x)+1)
+    :param X:
+    :param copy:
+    :return:
+    '''
+    if copy:
+        X = np.copy(X)
     prob = X
     prob *= -1
     np.exp(prob, prob)
     prob += 1
     np.reciprocal(prob, prob)
     return prob
+# 特点：
+# 1）- 若X不小于3，则prob不小于0.95. 是否适用于点击/余弦相似度？
 
 
 # examples:
 X = np.array([3.89, 3.13, 2.22, -1.2, 20.3, -3.33])
-softmax(X)
+softmax(X).round(4)
+# array([0.98  , 0.9581, 0.902 , 0.2315, 1.    , 0.0346])
+np.reciprocal(np.exp(-(3))+1)
+# 0.9525741268224334
+np.reciprocal(np.exp(-(-3))+1)
+# 0.04742587317756678
 
 
 # 2, Just use the softmax value as probability
@@ -53,18 +68,22 @@ def softmax2(X, v, copy=True):
     X[X < 0] = 0  # Filter the negative value to keep the output in [0, 1]
     max_prob = v
     X -= max_prob
-    return np.exp(X, X)
+    np.exp(X, X)
+    return X
 
 # counter examples:
 X = np.array([20.89, 22.13, 23.22, -15.2, 24.3, -20.33])
-softmax1(X, 24.3)
+softmax1(X, 24.3).round(4)
 # array([0.0222, 0.0768, 0.2284, 0.    , 0.6726, 0.    ])
-softmax2(X, 24.3)
+softmax2(X, 24.3).round(4)
 # array([0.033 , 0.1142, 0.3396, 0.    , 1.    , 0.    ])
 
 # 3, Just use the similarity value [-1, 1]. Discard the negative part, and use it as probability.
 
 
-
-
-
+# Attention - difference between np.exp(x)/np.reciprocal(x) and np.exp(x,x)/np.reciprocal(x,x)
+# 1, np.exp(x,x) changed the original value of x!!!
+# 2, np.exp(x,x) x should be of ArrayType
+x=np.array([2.1,1.3,4.2,3.5])
+assert np.alltrue(np.exp(x) == np.exp(x,x))
+assert np.alltrue(x == np.array([2.1,1.3,4.2,3.5]))
